01aae8057c01141627ddf4a54ca2684f6795e504
aaron <oneinstep@gmail.com>
2024-05-25 17:15:30 +0800

    init

---
+++ k8s-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      message: ''
+    };
+  },
+  mounted() {
+    fetch('/api')
+        .then(response => response.text())
+        .then(data => {
+          this.message = data;
+        });
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+}
+</style>
