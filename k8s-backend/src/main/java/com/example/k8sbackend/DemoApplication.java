3179aade228093e468aedf326e27c4454c525311
aaron <oneinstep@gmail.com>
2024-05-26 14:25:12 +0800

    1

--- k8s-backend/src/main/java/com/example/k8sbackend/DemoApplication.java
+++ k8s-backend/src/main/java/com/example/k8sbackend/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.k8sbackend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -15,6 +17,7 @@ public class DemoApplication {
 
     @GetMapping("/api")
     public String hello() {
+        log.info("get request to /api");
         return "Hello from backend!";
     }
 }
